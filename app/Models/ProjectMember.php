ca19d26c022feb5e13d8f9bcaba3fe4947f32ee1
Nada <nada.islamy@mhs.politala.ac.id>
2025-12-11 20:43:24 +0800

    Update detail dosen

--- app/Models/ProjectMember.php
+++ app/Models/ProjectMember.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProjectMember extends Model
 {
     use HasFactory;
     protected $fillable = ['project_id','user_id','peran'];
+}
+ public function project(): BelongsTo
+    {
+        return $this->belongsTo(\App\Models\ResearchProject::class, 'project_id', 'id');
+    }
 }
